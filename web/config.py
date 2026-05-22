4de867349c2fe4e2ef91012462af7d273680ca0b
CHUANHU-MIAO <chuanhumiao@gmail.com>
2026-05-22 12:23:21 +0800

    添加服务器部署配置

---
+++ web/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    BASEDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASEDIR, "instance", "system.db")}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = '/data/上传的文件'
+    SECRET_KEY = '123456'
+    STANDARD_FILES_FOLDER = os.path.join(UPLOAD_FOLDER, 'standard_files')
+    VERSION = 'V5.0.1'
