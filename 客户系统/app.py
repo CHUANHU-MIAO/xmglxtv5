898f3c74cb302c0693c4de31bf04d1781d4a9742
CHUANHU-MIAO <chuanhumiao@Gmail.com>
2026-06-22 16:32:35 +0800

    更改加载速度

--- 客户系统/app.py
+++ 客户系统/app.py
@@ -8,4 +8,11 @@ app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5005))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    use_waitress = os.environ.get('USE_FLASK_DEV', '').lower() not in ('1', 'true', 'yes')
+    if use_waitress:
+        from waitress import serve
+        print(f'[waitress] 多线程模式启动 http://0.0.0.0:{port} (threads=8)')
+        serve(app, host='0.0.0.0', port=port, threads=8)
+    else:
+        print(f'[Flask] 开发模式启动 http://0.0.0.0:{port}')
+        app.run(host='0.0.0.0', port=port, debug=True)
